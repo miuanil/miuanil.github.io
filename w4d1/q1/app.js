524a237134498c28279bdd02f4d2f07fbdd12c99
Anıl Özmen <me@anilozmen.com>
2022-10-24 11:20:51 -0500

    W4D1 Q1

---
+++ w4d1/q1/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res, next) => {
+
+    console.log(req.cookies)
+
+    res.render('form', {
+        pageTitle: 'Cookie Form',
+        bootstrapCDN: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css',
+        postURL: '/addCookie',
+        cookies: req.cookies,
+    });
+});
+
+app.post('/addCookie', (req, res, next) => {
+
+    if (req.body.key) {
+        res.cookie(req.body.key, req.body.value);
+    }
+
+    res.redirect('back');
+});
+
+app.listen(3000);
